870d8a36fa21278231c3feb0d05c44bf4b1b0477
anulrajeev <anulrajeev@gmail.com>
2020-07-19 10:22:52 +0530

    adding values

---
+++ index.js
@@ -0,0 +1,26 @@
+var rect=
+{
+    perimeter : (x,y)=>2*(x+y),
+    area      : (x,y)=>x*y
+
+};
+
+
+function rectangle(a,b)
+{
+   if(a<=0||b<=0)
+      console.log("The dimensions have to be a positive number");
+
+    else
+          {
+            console.log(rect.area(a,b));
+            console.log(rect.perimeter(a,b));
+          }
+}
+
+
+
+rectangle(3,4);
+rectangle(3,0);
+rectangle(3,-1);
+rectangle(3,5);
